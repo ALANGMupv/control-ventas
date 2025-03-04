247942a351fe5ee60af204841ce618a23c5865c4
ALANGMupv <alanguevara36@gmail.com>
2025-03-04 12:47:24 +0100

    feat: Simular login

--- src/app/index.php
+++ src/app/index.php
@@ -1,1 +1,10 @@
 <?php
+if ( empty ($_POST['usuario'])) {
+    header('Location: ../'); //Redirige al nivel superior, en este caso el index
+    /* http_response_code(401); // En vez del código de respuesta 200, forzar a un 401, no autorizado */
+}
+?>
+
+<h1>Bienvenido <?php echo $_POST['usuario']; ?></h1>
+
+
