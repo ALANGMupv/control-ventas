b27dbce77105ede0fe0df7d35f403dc2b9a84fe6
ALANGMupv <alanguevara36@gmail.com>
2025-03-11 12:44:45 +0100

    feat: Login simple

--- src/app/index.php
+++ src/app/index.php
@@ -1,8 +1,31 @@
 <?php
-if ( empty ($_POST['usuario'])) {
-    header('Location: ../'); //Redirige al nivel superior, en este caso el index
-    /* http_response_code(401); // En vez del código de respuesta 200, forzar a un 401, no autorizado */
+if ( empty ($_POST['usuario']) ||  empty ($_POST['password'])) {
+    header('Location: ../');
 }
+
+// region --- Conexión a la BBDD ---
+$servername = "localhost"; // Para plesk también es localhost
+$username = "root"; // El usuario que usamos por defecto en Xampp es root, en plesk lo crearemos nosotros
+$password = "";
+$dbname = "aguemar_control-ventas";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) { // Esto es un objeto, como en otra lenguaje sería conn(objeto).connect_error(propiedad)
+    die("Connection failed: " . $conn->connect_error); // El punto en php, es concatenación de cadenas
+}
+
+// endregion
+
+$sql = "SELECT * FROM `usuarios` WHERE `nombre_usuario` = '" . $_POST['usuario'] . "' AND `password` = '" . $_POST['password'] . "'";
+$result = $conn->query($sql); // almaceno el resultado de la consulta
+
+if ($result->num_rows == 0) {
+    header('Location: ../');
+}
+
 ?>
 
 <h1>Bienvenido <?php echo $_POST['usuario']; ?></h1>
